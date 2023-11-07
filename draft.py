ce29b17119834ae0f8646857ff25d4ac0cbc06ac
RafaLG18 <rafaellealg18@gmail.com>
2023-11-07 10:12:22 -0300

    #1: adding insert_rows

--- draft.py
+++ draft.py
@@ -1,7 +1,8 @@
 from functions.create_db import create_db
 from functions.create_table import create_table
-
-create_db('database.db')
-create_table(database_name="database.db",
-             table_name="tabela_amada",
-             columns="id INTERER PRIMARY KEY, nome_flor TEXT, qtd_petala_flor INTEGER")
+from functions.insert_rows import insert_one_row
+insert_one_row(database_name="database.db",
+               table_name="tabela_amada",
+               columns_name="id,nome_flor,qtd_petala_flor",
+               values="2,'copo_de_leite',3")
+#############
