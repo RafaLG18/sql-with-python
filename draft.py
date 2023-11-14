104143015e91ca114aff7555164129f6c0aa3534
RafaLG18 <rafaellealg18@gmail.com>
2023-11-14 09:15:46 -0300

    #8: add insert_many_rows

--- draft.py
+++ draft.py
@@ -1,8 +1,23 @@
 from functions.create_db import create_db
 from functions.create_table import create_table
 from functions.insert_rows import insert_one_row
+from functions.insert_rows import insert_many_rows
 insert_one_row(database_name="database.db",
                table_name="tabela_amada",
                columns_name="id,nome_flor,qtd_petala_flor",
                values="2,'copo_de_leite',3")
 #############
+#import sqlite3
+#
+#conn = sqlite3.connect("database.db")
+#cursor = conn.cursor()
+#lista = [(6,'flor do deserto',4),
+#    (7,'flor do deserto',5),
+#    (8,'flor do deserto',6)]
+#cursor.executemany("INSERT INTO tabela_amada (id,nome_flor,qtd_petala_flor) VALUES (?,?,?)",lista)
+#conn.commit()
+#conn.close()
+insert_many_rows(database_name="database.db",
+               table_name="tabela_amada",
+               columns_name="id,nome_flor,qtd_petala_flor",
+               list_values=[(9,'flor do deserto',4),(10,'flor do deserto',5),(11,'flor do deserto',6)])
--- functions/insert_rows.py
+++ functions/insert_rows.py
@@ -11,6 +11,12 @@ def insert_one_row(
         table_name (str): table name 
         columns_name (str): A query specifying the columns
         values (str): Values to insert
+        
+       Example:
+        insert_one_row(database_name="database.db",
+               table_name="tabela_amada",
+               columns_name="id,nome_flor,qtd_petala_flor",
+               values="2,'copo_de_leite',3")
     """
     conn=sqlite3.connect(database_name)
     cursor=conn.cursor()
@@ -21,3 +27,31 @@ def insert_one_row(
     )
     conn.commit()
     conn.close()
+    
+def insert_many_rows(database_name: str,
+    table_name:str,
+    columns_name:str,
+    list_values: list) -> None:
+    """ Create
+       Args:
+        database_name (str): created database name 
+        table_name (str): table name 
+        columns_name (str): A query specifying the columns
+        list_values (list): List of values to insert
+        
+       Example:
+        insert_many_rows(database_name="database.db",
+               table_name="tabela_amada",
+               columns_name="id,nome_flor,qtd_petala_flor",
+               list_values=[(9,'flor do deserto',4),(10,'flor do deserto',5),(11,'flor do deserto',6)])
+    """
+    conn = sqlite3.connect(database_name)
+    cursor = conn.cursor()
+    cursor.executemany(
+        f"""
+        INSERT INTO {table_name} ({columns_name}) VALUES (?,?,?)
+        """,list_values
+    )
+    conn.commit()
+    conn.close()
+
